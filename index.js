0999731965b8a617bf5699ff4813c64ee10afe7d
Eugene <eumorg@yandex.ru>
2022-09-16 00:35:27 +0700

    Final release

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const path = require('path');
-
+require("dotenv").config();
 
 const TelegramApi = require("node-telegram-bot-api");
 
-const sequelize = require('./db')
-
-
-
-// const { User } = require('./db/models');
-
-// const User = require('./db/models/')
-
-require('dotenv').config();
+const { User } = require("./db/models");
 
 const weather = require("weather-js");
 
@@ -22,20 +12,21 @@ const token = "5408753604:AAFD4CFICjgSm3Vk1WobiTHAuJuIwcQP2ug";
 
 const bot = new TelegramApi(token, { polling: true });
 
-const chats = {};
-
-const againOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: "Play again", callback_data: "/again" }]],
-  }),
-};
-
 const gameOptions = {
   reply_markup: JSON.stringify({
     inline_keyboard: [
       [{ text: "Погода в Канаде", callback_data: "1" }],
       [{ text: "Хочу почитать!", callback_data: "2" }],
       [{ text: "Сделать рассылку", callback_data: "3" }],
+    ],
+  }),
+};
+
+const sendOptions = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: "Уверен", callback_data: "4" }],
+      [{ text: "Отмена", callback_data: "5" }],
     ],
   }),
 };
@@ -48,60 +39,50 @@ const startGame = async (chatId) => {
   );
 };
 
-const start = async() => {
-  try {
-    await sequelize.authenticate()
-    await sequelize.sync()
-  } catch (error) {
-    console.log(error)
-    
-  }
-  bot.setMyCommands([
-    { command: "/start", description: "Начало работы" },
-    // {command: '/info', description: 'Getting users info'},
-    // {command: '/game', description: 'play a game'}
-  ]);
+const start = async () => {
+  bot.setMyCommands([{ command: "/start", description: "Начало работы" }]);
 
   bot.on("message", async (msg) => {
     const text = msg.text;
-    const chatId = msg.chat.id;
+
+    const userName = msg.chat.username;
+    const chatId = String(msg.chat.id);
     if (text === "/start") {
       try {
-        await User.create({
-          userName: 'Biba',
-          chatId: 'ddd'
-        })
+        await User.findOrCreate({
+          where: {
+            userName: userName,
+            chatId: chatId,
+          },
+        });
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-      
+
       return startGame(chatId);
     }
-    // if(text === '/info'){
-    //   return bot.sendMessage(chatId, `Your name is ${msg.from.first_name}`)
-    // }
-    // if(text === '/game'){
-    //   return startGame(chatId)
-    // }
-    return bot.sendMessage(chatId, "Can not understand");
   });
 
   bot.on("callback_query", async (msg) => {
     const data = msg.data;
-    console.log(data);
+
     const chatId = msg.message.chat.id;
     if (data == 1) {
       weather.find(
         { search: "Toronto, ON", degreeType: "C" },
-        function (err, result) {
+        async function (err, result) {
           if (err) console.log(err);
 
-          // result = JSON.stringify(result, null, 2);
           let temp = result[0].current.temperature;
 
-          return bot.sendMessage(
+          await bot.sendMessage(
             chatId,
             `Температура в Торонто, Канада ${temp} C`
+          );
+          return bot.sendMessage(
+            chatId,
+            "Нажмите на любую интересующую Вас кнопку",
+            gameOptions
           );
         }
       );
@@ -116,9 +97,52 @@ const start = async() => {
             "Идеальный карманный справочник для быстрого ознакомления с особенностями работы разработчиков на Python. Вы найдете море краткой информации о типах и операторах в Python, именах специальных методов, встроенных функциях, исключениях и других часто используемых стандартных модулях.",
         }
       );
-      return bot.sendDocument(
+      await bot.sendDocument(
         chatId,
         `https://drive.google.com/u/0/uc?id=1Xs_YjOLgigsuKl17mOnR_488MdEKloCD&export=download`
+      );
+      return bot.sendMessage(
+        chatId,
+        "Нажмите на любую интересующую Вас кнопку",
+        gameOptions
+      );
+    }
+    if (data == 3) {
+      return bot.sendMessage(
+        chatId,
+        "Вы выбрали рассылку всем пользователям. Вы уверен что хотите это сделать?",
+        sendOptions
+      );
+    }
+    if (data == 4) {
+      bot.sendMessage(
+        chatId,
+        "Введите сообщение, которое хотите отправить всем пользователям."
+      );
+      bot.on("message", async (msg) => {
+        try {
+          const text = msg.text;
+          ids = await User.findAll({ raw: true });
+          ids = ids.map((el) => el.chatId);
+
+          for (let i = 0; i < ids.length; i++) {
+            bot.sendMessage(ids[i], text);
+            return bot.sendMessage(
+              chatId,
+              "Рассылка завершена! Нажмите на любую интересующую Вас кнопку",
+              gameOptions
+            );
+          }
+        } catch (error) {
+          console.log("Ошибка рассылки", error);
+        }
+      });
+    }
+    if (data == 5) {
+      return bot.sendMessage(
+        chatId,
+        "Нажмите на любую интересующую Вас кнопку",
+        gameOptions
       );
     }
   });
@@ -128,5 +152,5 @@ start();
 
 const port = process.env.PORT || 3100;
 app.listen(port, () => {
-  console.log('\x1b[33m', `Server started at http://localhost:${port}`);
+  console.log("\x1b[33m", `Server started at http://localhost:${port}`);
 });
